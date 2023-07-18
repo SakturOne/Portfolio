b7b5a34ef8d9cc8cc97bd1a0f112d9f1284ebe0a
SakturOne <victor.ponce.paez@gmail.com>
2023-07-18 14:07:03 +0200

    Actualizacion interface y registro

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,60 @@ import Header from "./Componentes/Atomos/ComponentesHeader/Header";
 import About from "./Componentes/Atomos/ComponentesHeader/ComponentesAbout/About";
 import Projects from "./Componentes/Atomos/ComponentesHeader/ComponentesProyects/Proyects";
 import Footer from "./Componentes/Atomos/ComponentesHeader/ComponentesFooter/Footer";
+import Login from "./Componentes/Atomos/ComponentesHeader/ComponentesLoging/Loging";
+
+
+
 
 const App = () => {
   return (
 
     <main className="h-screen bg-black">
-      <div>
-        <Header />
 
-        <Projects/>
-        <About/>
-        <Footer/>
-        
-        {/* u contenido principal aquí */}
-
+      <div className="rounded-lg" >
+        <Login />
       </div>
 
+      <div>
+        <Header />
+      </div>
+      <div>
+        <About />
+      </div>
+
+      <div>
+        <Projects />
+      </div>
+
+
+      
+
+      <div>
+        <Footer />
+      </div>
+      {/* u contenido principal aquí */}
+
     </main>
-
-
-);
+  );
 };
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
